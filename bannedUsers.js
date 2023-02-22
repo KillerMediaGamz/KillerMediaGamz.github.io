e40873a69c5bc0aca84a834b9ccd0f1f0852131f
KM <124732203+KillerMediaGamz@users.noreply.github.com>
2023-02-21 18:09:48 -0700

    Update bannedUsers.js

--- bannedUsers.js
+++ bannedUsers.js
@@ -1,1 +1,10 @@
+<script type="application/javascript">
+  function getIP(json) {
+    if (json.ip = "73.181.79.140") {
+     alert("You are banned from this site, go play with google.");
+        window.location = "https://www.google.com";
+    }
+  }
+</script>
 
+<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>

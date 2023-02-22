2fe4f8b458a4a1dc6f12fddcf38cb2c1ce582652
KM <124732203+KillerMediaGamz@users.noreply.github.com>
2023-02-21 18:34:07 -0700

    Update bannedUsers.js

--- bannedUsers.js
+++ bannedUsers.js
@@ -1,10 +1,20 @@
-<script type="application/javascript">
-  function getIP(json) {
-    if (json.ip = "73.181.79.140") {
-     alert("You are banned from this site, go play with google.");
-        window.location = "https://www.google.com";
-    }
-  }
-</script>
+<script type="text/javascript">
 
-<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
+// Block IP address script- By JavaScriptKit.com (http://www.javascriptkit.com)
+// For this and over 400+ free scripts, visit JavaScript Kit- http://www.javascriptkit.com/
+// This notice must stay intact for use.
+
+//Enter list of banned ips, each separated with a comma:
+var bannedips=["73.181.79.140", "11.11.11.11"]
+
+var ip = '<!--#echo var="REMOTE_ADDR"-->'
+
+var handleips=bannedips.join("|")
+handleips=new RegExp(handleips, "i")
+
+if (ip.search(handleips)!=-1){ 
+alert("Your IP has been banned from this site. Redirecting...")
+window.location.replace("http://www.google.com")
+}
+
+</script>
